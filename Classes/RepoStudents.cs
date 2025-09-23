e1ba0a8c359bcee2adb1627d5ae7a855472cff8f
TeHe4K <hryblackwii@gmail.com>
2025-09-23 14:47:33 +0500

    Разработанный класс RepoStudents. Коневский

---
+++ Classes/RepoStudents.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Konevskii.Classes
+{
+    public class RepoStudents
+    {
+        public static List<Student> AllStudent()
+        {
+            List<Student> allStudent = new List<Student>();
+
+            allStudent.Add(new Student("Болотов", "Роман", "Владимирович"));
+            allStudent.Add(new Student("Григорьев", "Евгений", "Олегович"));
+            allStudent.Add(new Student("Гудков", "Георгий", "Константинович", false, 3));
+            allStudent.Add(new Student("Исыпова", "Алёна", "Александровна", true));
+            allStudent.Add(new Student("Иутин", "Павел", "Алексеевич", false, 3));
+            allStudent.Add(new Student("Ишимов", "Виктор", "Алексеевич"));
+            allStudent.Add(new Student("Калюжный", "Артам", "Евгеньевич"));
+            allStudent.Add(new Student("Кусакина", "Полина", "Олеговна", true));
+            allStudent.Add(new Student("Ленченков", "Александр", "Дмитриевич"));
+            allStudent.Add(new Student("Лесникова", "Мария", "Михайловна", true));
+            allStudent.Add(new Student("Лихачева", "Татьяна", "Яковлевна"));
+            allStudent.Add(new Student("Мокрушина", "Надежда", "Владимировна", true));
+            allStudent.Add(new Student("Мутагаров", "Даниил", "Ринатович"));
+            allStudent.Add(new Student("Нарижный", "Данил", "Владленович"));
+            allStudent.Add(new Student("Никонов", "Арсений", "Дмитриевич", false, 3));
+            allStudent.Add(new Student("Оборин", "Даниил", "Артёмович"));
+            allStudent.Add(new Student("Посадских", "Дарья", "Андреевна"));
+            allStudent.Add(new Student("Сторожев", "Денис", "Романович", true));
+            allStudent.Add(new Student("Суслов", "Егор", "Владимирович"));
+            allStudent.Add(new Student("Токмаков", "Даниил", "Сергеевич", true));
+            allStudent.Add(new Student("Тронин", "Александр", "Владиславович"));
+            allStudent.Add(new Student("Халилов", "Дамит", "Ринатович"));
+            allStudent.Add(new Student("Шестаков", "Дмитрий", "Андреевич"));
+
+            return allStudent;
+        }
+    }
+}
