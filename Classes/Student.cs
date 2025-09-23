262f78a5dfb6cdefda52921f1e1d3396ee450944
TeHe4K <hryblackwii@gmail.com>
2025-09-23 14:42:01 +0500

    Разработанный класс Student. Коневский

---
+++ Classes/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Konevskii.Classes
+{
+    public class Student
+    {
+        public string Firstname = "";
+        public string Lastname = "";
+        public string Surname = "";
+        public bool Scholarship = false;
+        public int Course = 4;
+
+        public Student(string Firstname, string Lastname, string Surname)
+        {
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+            this.Surname = Surname;
+        }
+
+        public Student(string Firstname,string Lastname, string Surname, bool Scholarship):this(Firstname, Lastname, Surname)
+        {
+            this.Scholarship = Scholarship;
+        }
+
+        public Student(string Firstname, string Lastname, string Surname, bool Scholarship, int Course) : this(Firstname, Lastname, Surname, Scholarship)
+        {
+            this.Course = Course;
+        }
+
+        public string GetFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
+    }
+}
